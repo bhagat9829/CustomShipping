a3cd58fca73ee918c6a2a8f031e42791b17fd47e
bhagat9829 <94214927+bhagat9829@users.noreply.github.com>
2021-11-22 12:18:14 +0530

    Add files via upload

---
+++ CustomShipping/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Copyright © Developer AJAY BHAGAT Dotsquares
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Dotsquares_CustomShipping',
+    __DIR__
+);
